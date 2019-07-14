106b2b778d8134c79a481e90f00aa15871ae0ad5
Casper Meloen <cemeloen@gmail.com>
2019-07-15 01:50:28 +0200

    Simple Player class with a test movement module

---
+++ FirstPersonShooterCharacterControllerRemake/Assets/Scripts/Features/Player/Player.cs
@@ -0,0 +1,15 @@
+﻿using CM.Essentials;
+using UnityEngine;
+
+public class Player : Entity, IMovement2D
+{
+	public void Move(Vector2 input)
+	{
+		IMovement2D[] modules = GetModules<IMovement2D>();
+
+		foreach (IMovement2D module in modules)
+		{
+			module.Move(input);
+		}
+	}
+}
