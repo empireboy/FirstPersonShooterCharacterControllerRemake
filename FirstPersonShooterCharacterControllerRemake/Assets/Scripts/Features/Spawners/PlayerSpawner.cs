85e4ec4599416883dedbe3fd097384f0f0549278
Casper Meloen <cemeloen@gmail.com>
2019-08-08 01:27:14 +0200

    Added a Spawner scene and Spawner class

---
+++ FirstPersonShooterCharacterControllerRemake/Assets/Scripts/Features/Spawners/PlayerSpawner.cs
@@ -0,0 +1,28 @@
+﻿using CM.Essentials;
+using CM.Orientation;
+using CM.Spawner;
+using UnityEngine;
+
+public class PlayerSpawner : Spawner
+{
+	[SerializeField]
+	private GameObject _movementManager;
+
+	[SerializeField]
+	private GameObject _rotationManager;
+
+	private void Start()
+	{
+		Spawn();
+	}
+
+	protected override void OnSpawn(GameObject instantiatedObject)
+	{
+		base.OnSpawn(instantiatedObject);
+
+		Player player = instantiatedObject.GetComponent<Player>();
+
+		_movementManager.GetComponent<Manager<IMovement2D>>().interfaces.Add(player);
+		_rotationManager.GetComponent<Manager<IRotate>>().interfaces.Add(player);
+	}
+}
