03d694fda25f4e6483660487b3ca452831f0dac8
Casper Meloen <cemeloen@gmail.com>
2019-08-30 15:59:36 +0200

    Added a PlayerSpawnTestManager

---
+++ FirstPersonShooterCharacterControllerRemake/Assets/Scripts/Managers/PlayerSpawnTestManager.cs
@@ -0,0 +1,30 @@
+﻿using CM.Essentials;
+using CM.Orientation;
+using UnityEngine;
+
+public class PlayerSpawnTestManager : MonoBehaviour
+{
+	[SerializeField]
+	private Transform _spawnPoint;
+
+	[SerializeField]
+	private Player _playerPrefab;
+
+	[Header("Managers")]
+
+	[SerializeField]
+	private GameObject _movementManager;
+
+	[SerializeField]
+	private GameObject _rotationManager;
+
+	private void Start()
+	{
+		Player player = Instantiate(_playerPrefab);
+		player.SetSpawn(_spawnPoint);
+		player.Spawn();
+
+		_movementManager.GetComponent<Manager<IMovement2D>>().AddInterface(player);
+		_rotationManager.GetComponent<Manager<IRotate>>().AddInterface(player);
+	}
+}
